2d413616ce2f2721d40104685a375733a1698577
Remco Visser <remco.visser@maximum.nl>
2018-03-30 23:09:26 +0200

    week 2

--- dest/app.js
+++ dest/app.js
@@ -1,9 +1,81 @@
-var Fun = function (f) { return { f: f }; };
-// let double = Fun<number,number>(x => x * 4)
-// let aaa = double.f(10);
-var x = 10;
-var y = 20;
-var then = function (f, g) {
+// -------------- Week 1 -------------- //
+var __assign = (this && this.__assign) || Object.assign || function(t) {
+    for (var s, i = 1, n = arguments.length; i < n; i++) {
+        s = arguments[i];
+        for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+            t[p] = s[p];
+    }
+    return t;
+};
+var Fun = function (f) {
+    return {
+        f: f,
+        then: function (g) {
+            return then_operator(this, g);
+        }
+    };
+};
+var then_operator = function (f, g) {
     return Fun(function (a) { return g.f(f.f(a)); });
 };
+var id = function (x) {
+    return Fun(function (x) { return x; });
+};
+var incr = Fun(function (x) { return x + 1; });
+var double = Fun(function (x) { return x * 2; });
+var negate = Fun(function (x) { return !x; });
+var is_even = Fun(function (x) { return x % 2 == 0; });
+var convert = Fun(function (x) { return String(x); });
+var ifThenElse = function (p, _then, _else) {
+    return Fun(function (x) {
+        if (p.f(x)) {
+            return _then.f(x);
+        }
+        else {
+            return _else.f(x);
+        }
+    });
+};
+var xx = ifThenElse(is_even, double, incr);
+var incr_then_double = incr.then(double);
+var incr_twice = incr.then(incr);
+var double_twice = double.then(double);
+var my_f = incr.then(is_even);
+var x = incr.then(double).f(5);
+var y = incr.then(double).then(convert).f(5);
+var map_countainer = function (f) {
+    return Fun(function (c) { return ({ content: f.f(c.content), counter: c.counter }); });
+};
+var tick = Fun(function (c) { return (__assign({}, c, { counter: c.counter + 1 })); });
+var incr_countainer = map_countainer(incr);
+var is_countainer_even = map_countainer(is_even);
+var countainer = { content: 3, counter: 0 };
+var a = incr_countainer.f(countainer);
+var b = incr_countainer.then(incr_countainer).then(tick).f(countainer);
+var none = function () { return { kind: "none" }; };
+var some = function (x) { return { kind: "some", value: x }; };
+var map_Option = function (f) {
+    return Fun(function (x) { return x.kind == "none" ? none() : some(f.f(x.value)); });
+};
+function printOption(x) {
+    if (x.kind == "some")
+        return "the value is " + x.value;
+    else
+        return "there is no value";
+}
+var pipeline = map_Option(incr.then(double.then(incr)));
+var option = { kind: "some", value: 2 };
+var c = pipeline.f(option);
+var map_Id = function (f) {
+    return f;
+};
+var d = map_Id(incr).f(4);
+var e = incr.f(4);
+var map_Countainer_Maybe = function (f) {
+    return map_countainer(map_Option(f));
+};
+var countainerOption = { content: { kind: "some", value: 2 }, counter: 0 };
+var countainerMaybe = countainerOption;
+var ee = map_Countainer_Maybe(incr).f(countainerOption).content;
+console.log(printOption(ee));
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,116 @@
-type Fun<a,b> = { f:(i:a) => b }
+// -------------- Week 1 -------------- //
 
-let Fun = function<a,b>(f:(_:a) => b) : Fun<a,b> {  return { f:f } }
-
-// let double = Fun<number,number>(x => x * 4)
-// let aaa = double.f(10);
-
-let x = 10;
-let y = 20;
-
-
-let then = function<a,b,c>(f:Fun<a,b>, g:Fun<b,c>) : Fun<a,c> {
-    return Fun<a,c>(a => g.f(f.f(a)))
+type Fun<a, b> = {
+  f: (i: a) => b,
+  then: <c>(g: Fun<b, c>) => Fun<a, c>
 }
+
+let Fun = function <a, b>(f: (val: a) => b): Fun<a, b> {
+  return {
+    f: f,
+    then: function <c>(this: Fun<a, b>, g: Fun<b, c>): Fun<a, c> {
+      return then_operator(this, g)
+    }
+  }
+}
+
+let then_operator = function <a, b, c>(f: Fun<a, b>, g: Fun<b, c>): Fun<a, c> {
+  return Fun<a, c>(a => g.f(f.f(a)))
+}
+
+let id = function <a>(x): Fun<a, a> {
+  return Fun<a, a>(x => x)
+}
+
+
+let incr = Fun<number, number>(x => x + 1)
+let double = Fun<number, number>(x => x * 2)
+let negate = Fun<boolean, boolean>(x => !x)
+let is_even = Fun<number, boolean>(x => x % 2 == 0)
+let convert = Fun<number, string>((x: number) => String(x))
+
+let ifThenElse =
+  function <a, b>(p: Fun<a, boolean>, _then: Fun<a, b>, _else: Fun<a, b>): Fun<a, b> {
+    return Fun((x: a) => {
+      if (p.f(x)) {
+        return _then.f(x)
+      }
+      else {
+        return _else.f(x)
+      }
+    })
+  }
+
+let xx = ifThenElse(is_even, double, incr);
+
+
+let incr_then_double = incr.then(double)
+let incr_twice = incr.then(incr)
+let double_twice = double.then(double)
+let my_f = incr.then(is_even)
+
+
+let x = incr.then(double).f(5);
+let y = incr.then(double).then(convert).f(5)
+
+
+
+
+
+// -------------- Week 2 -------------- //
+
+type Countainer<a> = { content: a, counter: number }
+
+let map_countainer = function <a, b>(f: Fun<a, b>): Fun<Countainer<a>, Countainer<b>> {
+  return Fun(c => ({ content: f.f(c.content), counter: c.counter }))
+}
+
+let tick: Fun<Countainer<number>, Countainer<number>> = Fun(c => ({ ...c, counter: c.counter + 1 }))
+
+let incr_countainer: Fun<Countainer<number>, Countainer<number>> = map_countainer(incr)
+let is_countainer_even: Fun<Countainer<number>, Countainer<boolean>> = map_countainer(is_even)
+
+let countainer: Countainer<number> = { content: 3, counter: 0 }
+let a = incr_countainer.f(countainer);
+let b = incr_countainer.then(incr_countainer).then(tick).f(countainer);
+
+
+type Option<a> = { kind: "none" } | { kind: "some", value: a }
+let none = function <a>(): Option<a> { return { kind: "none" } }
+let some = function <a>(x: a): Option<a> { return { kind: "some", value: x } }
+
+let map_Option = function <a, b>(f: Fun<a, b>): Fun<Option<a>, Option<b>> {
+  return Fun(x => x.kind == "none" ? none<b>() : some<b>(f.f(x.value)))
+}
+
+function printOption(x: Option<number>): string {
+  if (x.kind == "some")
+    return `the value is ${x.value}`
+  else
+    return "there is no value"
+}
+
+let pipeline: Fun<Option<number>, Option<number>> = map_Option(incr.then(double.then(incr)))
+
+let option: Option<number> = { kind: "some", value: 2 };
+let c = pipeline.f(option);
+
+
+type Id<a> = a
+let map_Id = function <a, b>(f: Fun<a, b>): Fun<Id<a>, Id<b>> {
+  return f
+}
+
+let d = map_Id(incr).f(4);
+let e = incr.f(4);
+
+type CountainerMaybe<a> = Countainer<Option<a>>
+let map_Countainer_Maybe = function <a, b>(f: Fun<a, b>): Fun<CountainerMaybe<a>, CountainerMaybe<b>> {
+  return map_countainer<Option<a>, Option<b>>(map_Option<a, b>(f));
+}
+
+
+let countainerOption: Countainer<Option<number>> = { content: { kind: "some", value: 2 }, counter: 0 }
+let countainerMaybe: CountainerMaybe<number> = countainerOption;
+let ee = map_Countainer_Maybe(incr).f(countainerOption).content;
+console.log(printOption(ee));
